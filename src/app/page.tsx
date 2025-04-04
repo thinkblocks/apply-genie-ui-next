897636849d8caa92157a79159bda85e2e5f298e2
thinkblocks <econnectandsales@gmail.com>
2025-04-04 15:13:41 +0530

    Added prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import React from "react"
+import React from 'react';
 
 export default function Page() {
     return (
         <div>
             <h1>Hello, Next.js! 2.</h1>
         </div>
-    )
+    );
 }
